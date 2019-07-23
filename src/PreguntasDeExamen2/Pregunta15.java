834aa23a579f0f9c1d9a68a7ccd1d044ee40392e
LuisArmandosalem <luisarmandogomezjimenez@gmail.com>
2019-07-23 12:44:06 -0500

    nuevos cambios a gregados a las 12:43

---
+++ src/PreguntasDeExamen2/Pregunta15.java
@@ -0,0 +1,10 @@
+package PreguntasDeExamen2;
+
+public class Pregunta15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( 144  >>>  4); 
+	}
+
+}
