afa69e3f75ecbf01a3ea0d61e1393b7c46225e6f
LuisArmandosalem <luisarmandogomezjimenez@gmail.com>
2019-07-29 16:47:12 -0500

    se agrega una clase con los metodos de sincronizacion de thread

--- src/synchronizedEjemplo/Fichero.java
+++ src/synchronizedEjemplo/Fichero.java
@@ -2,11 +2,45 @@ package synchronizedEjemplo;
 
 public class Fichero {
 	public static void main(String[] args) {
-		int[] array = {2,3,4,5,6,5,3,2};
-		synchronized ( array) {
-			for (int i = 0; i < array.length; i++) {
-				System.out.println("arreglos: "+i);
-			}
+		// se crean la instasia del metodo 
+		Hilosvarios obj = new Hilosvarios();
+		Hilosvarios obj2 = new Hilosvarios();
+		// se ejeuta el hilo con el metodo star 
+		obj.start();
+		try {
+			// el metodo join obloga al hilo a terminar su secuencias 
+			obj.join();
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
 		}
+	
+		obj2.start();
+		try {
+			obj2.join();
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+		System.out.println("Sincornizacion terminsda");
+	}
+	
+}
+// se crea una clase la cual extiende de la clase Thread 
+class Hilosvarios extends Thread {
+	// se rescribe la clase run para ejecutar el metodo estar al invocarl la clase 
+	public void run() {
+		// se corre un arreglo para que este traiga un arreglos con varios parametros
+		 try {
+			 for (int i = 0; i < 15; i++) {
+				 System.out.println("Esto es un hilo"+ getName());
+				 // se crea este metodo para que en cada siclo espere 500 milisegundos con el metodo sleep
+				 Hilosvarios.sleep(500);
+			 }
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+		
 	}
 }
