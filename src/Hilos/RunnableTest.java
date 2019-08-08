de55908908d4bbc27d2f5d0105a4334beab7c3db
LuisArmandosalem <luisarmandogomezjimenez@gmail.com>
2019-08-08 11:03:05 -0500

    commitsasa

--- src/Hilos/RunnableTest.java
+++ src/Hilos/RunnableTest.java
@@ -5,7 +5,14 @@ public class RunnableTest {
 	public static void main(String[] args) {
 	 /// isntacion de un objeto
 		SinThread  obj= new SinThread();
+		Thread hilo = new Thread();
 		
+		try {
+			hilo.wait();
+		} catch (InterruptedException e) {
+	
+			e.printStackTrace();
+		}
 
 	}
 
